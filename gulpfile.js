4e11bf5f570e104451f856afc00476cf8d4c9b0b
afruitycat <saw14@aber.ac.uk>
2016-01-15 14:03:46 +0000

    changes to gulp and refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,14 @@
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
 
-//compiling Jade to HTML using gulp-jade
-gulp.task('html', function() {
-   gulp.src(['./app/**/*.jade'])
-   .pipe($.jade({pretty: true, doctype: 'html'}))
-   .on('error', $.util.log)
-   .pipe(gulp.dest('./public/'));
+/*Example*/
+gulp.task('copy', function() {
+  return gulp.src('copy.txt')
+    .pipe(gulp.dest('ng'));
 });
 
-//compile coffee script to javascript using gulp-coffee
-gulp.task('js', function() {
-  gulp.src(['./app/**/*.coffee'])
-  .pipe($.coffee({bare: true}))
-  .on('error', $.util.log)
-  .pipe(gulp.dest('./public/'));
+gulp.task('default', function () {
+  gulp.src('app/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./ng/'));
 });
-
-// create a TASK to compile Sass into CSS using gulp-sass
-gulp.task('css', function() {
-   gulp.src(['./app/**/*.scss'])
-   .pipe($.sass({style: 'expanded'}))
-   .pipe(gulp.dest('./public/'));
-});
-
-//"watch" task for any changes in your files and rerun gulp if necessary
-gulp.task('watch', function() {
-   gulp.watch(['./app/**/*.jade'], ['html']);
-   gulp.watch(['./app/**/*.coffee'], ['js']);
-   gulp.watch(['./app/**/*.scss'], ['css']);
-});
-
-gulp.task('default', ['html', 'js', 'css', 'watch'], function() {});
