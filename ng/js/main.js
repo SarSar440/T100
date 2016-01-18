96173c39bc2872836cd26c26f46da00f5ab77a84
afruitycat <saw14@aber.ac.uk>
2016-01-18 16:08:19 +0000

    added code for add and clear todo tasks

--- ng/js/main.js
+++ ng/js/main.js
@@ -4,7 +4,15 @@ app.controller('TodoAppController', [ '$scope', function($scope) {
     $scope.tasks = [ { 'title': 'build an app', 'done': false }];
     $scope.error = false;
 
-    $scope.addTask = function(){};
-    $scope.clearTask = function(){};
+    $scope.addTodo = function(){
+      $scope.tasks.push({'title' : $scope.newTodo, 'done':false});
+      $scope.newTodo = '';
+    };
+
+    $scope.clearTodo = function(){
+      $scope.tasks = $scope.tasks.filter(function(i){
+        return !i.done;
+      });
+    };
   }
 ]);
